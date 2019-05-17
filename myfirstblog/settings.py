f1741898471748bac9eed06cc27f11afc4660387
Prashant288 <prashantsemwal311@gmail.com>
2019-05-17 18:29:48 +0530

    Update settings.py

--- myfirstblog/settings.py
+++ myfirstblog/settings.py
@@ -147,10 +147,7 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles','static')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-  os.path.join(BASE_DIR,'static'),
 
-  )
 
 
 EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
