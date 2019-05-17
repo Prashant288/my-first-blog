fcfad190ecd05625acf9f8f18bf72aa2205a9546
Prashant288 <prashantsemwal311@gmail.com>
2019-05-17 18:53:40 +0530

    Update settings.py

--- myfirstblog/settings.py
+++ myfirstblog/settings.py
@@ -145,8 +145,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles','static')
+STATIC_ROOT = '/home/blogteek/blogteek.pythonanywhere.com/static'
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+  os.path.join(BASE_DIR,'static'),
+
+  )
 
 
 
